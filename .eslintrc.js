39449b52a61a6e86e3f4a0959ec192c2e74765cb
vellengs <vellengs@qq.com>
2019-05-12 15:24:59 +0800

    add prettie

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,26 +11,23 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       sourceType: 'module'
     },
+
+    extends: [
+      'standard', 
+      'plugin:vue/essential',
+      "plugin:prettier/recommended",
+    ], 
   
     plugins: [
       'vue'
     ],
   
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'space-before-function-paren': [2, 'never'],
-      'vue/array-bracket-spacing': 'error',
-      'vue/arrow-spacing': 'error',
-      'vue/block-spacing': 'error',
-      'vue/brace-style': 'error',
-      'vue/camelcase': 'error',
-      'vue/comma-dangle': 'error',
-      'vue/component-name-in-template-casing': 'error',
-      'vue/eqeqeq': 'error',
-      'vue/key-spacing': 'error',
-      'vue/match-component-file-name': 'error',
-      'vue/object-curly-spacing': 'error'
+      "prettier/prettier": "error",
+      // allow async-await
+      'generator-star-spacing': 'off',
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off' 
     },
   
     'extends': [
